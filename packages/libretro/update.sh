7eb64381a18359186aa8702de9ef3fa589fe94ae
Benjamin Pereto <benjamin@sandchaschte.ch>
2018-12-24 11:31:50 +0100

    CLEANUP: remove temp. shell script

--- packages/libretro/update.sh
+++
@@ -1,9 +0,0 @@
-for p in $(find . -name package.mk)
-do
-    PKG_URL=
-    . $p
-    echo "$PKG_GIT_URL"
-    PKG_URL="$PKG_GIT_URL/archive/\$PKG_VERSION.tar.gz"
-    echo $PKG_URL
-    echo "PKG_URL=\"$PKG_URL\"" >> $p
-done
